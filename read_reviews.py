3a22a090c3f62f77415062fb501f8d28b3b5d096
unknown <csfn05@csf.org.tw>
2019-01-25 17:11:39 +0800

    calc avg len of reviews

--- read_reviews.py
+++ read_reviews.py
@@ -6,7 +6,18 @@ with open('reviews.txt', 'r')as f:
 		count += 1
 		if count % 10000 == 0:#讀取進度
 			print(len(data))
+print('總共有', len(data),  '筆資料!')#文章筆數
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('留言平均長', sum_len / len(data))
 
-print(len(data))#文章筆數
-print(data[0])
-print(data[1])
+
+
+
+
+
+
+
+# print(data[0])
+# print(data[1])
